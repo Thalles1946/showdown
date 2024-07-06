bd8280791d31c94d6bd56551d355e62f8235997d
Thalles1946 <thallesaug2099@gmail.com>
2024-07-06 11:39:43 -0300

    Feat : Added About page Feat : Added Work page

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,4 +1,17 @@
+import "../App.css";
+
 function Work() {
-  return <>Work</>;
+  return (
+    <body className="App-body">
+      <h4 className="Body-subtitle">What I am doing now?</h4>
+      <p className="Body-description">
+        Actually I am working at Usiminas, creating and mantaining Java API's
+        using Quarkus. Besides, I am developing a mobile app using React Native
+        for frontend and for backend I am using NodeJS and PostgreSQL.
+        <br />I always like to work among a team, contributing actively with
+        developing collective acknowledge.
+      </p>
+    </body>
+  );
 }
 export default Work;
